e0b14b853fe3c8eeb0a22c4e284784c3a95cdc11
Intizam1993 <99119554+Intizam1993@users.noreply.github.com>
2022-06-07 23:57:10 +0400

    Update symbols.py

--- text/symbols.py
+++ text/symbols.py
@@ -7,6 +7,8 @@ The default is a set of ASCII characters that works well for English or text tha
 from text import cmudict
 
 _pad        = '_'
+_eos        = '~'
+_characters = 'ÜİÖĞƏÇŞüöğıəçş'
 _punctuation = '!\'(),.:;? '
 _special = '-'
 _letters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz'
@@ -15,4 +17,5 @@ _letters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz'
 _arpabet = ['@' + s for s in cmudict.valid_symbols]
 
 # Export all symbols:
+symbols = [_pad, _eos] + list(_characters) + _arpabet
 symbols = [_pad] + list(_special) + list(_punctuation) + list(_letters) + _arpabet
